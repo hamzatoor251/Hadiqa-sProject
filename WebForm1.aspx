4486e30d7cd54f5ec628ed20f44090dd7a460054
Muhammad Hamza Mushtaq <70532236+hamzatoor251@users.noreply.github.com>
2022-08-03 10:09:27 +0500

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,115 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Employee_Data.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Employee Data</title>
+    <%--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>--%>
+     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script>
+        
+        function Check() {
+            var err = document.getElementById("error");
+            var inpt = document.getElementById("filter").value;
+            var pattern = /^[A-Za-z]+$/;
+            var err = document.getElementById("error");
+            err.innerHTML = "";
+            if (!inpt.match(pattern)) {
+                var err = document.getElementById("error");             
+                err.innerHTML = "Enter a valid expression!";
+                
+            }
+            else {
+                var inpt = document.getElementById("filter").value;
+                $.ajax({
+                    url: 'WebForm1.aspx/DisplayOptions',
+                    type: 'post',
+                    data: JSON.stringify({ "userInput": inpt }),
+                    contentType: 'application/json',
+                    async: false,
+                    success: function (data) {
+                        var listOptions = document.getElementById("opt");
+                        let rand = "";
+                        for (let i = 0; i < (data.d).length; i++) {
+                            rand += '<option value="' + data.d[i] + '"></option>';
+                        }
+                        listOptions.innerHTML = '<datalist id="filter">' + rand + '</datalist>';
+                    }
+                });
+                 
+            }
+        }
+        
+            
+    </script>
+    <style>
+        #filter {
+           
+            padding:3px;
+            border:solid;
+            font-size:large;
+            width: 300px;
+            background-color:#caf0f8 ;
+            font-family:Helvetica;
+            margin-bottom: 3px;
+        }
+        #filterBtn {
+         
+            padding:3px;
+            border:solid;
+            font-size:large;
+            padding-left:5px;
+            padding-right:5px;
+            border-radius:3px;
+            font-family:Helvetica;
+            margin-bottom: 10px;
+        
+        }
+        .clear {
+            clear:both;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            
+            <asp:TextBox runat="server" ID="filter" placeholder="Filter by Department" OnKeyUp=" Check()"></asp:TextBox><br />
+            <div id="opt">
+
+            </div>
+            <asp:Label runat="server" id="error" style="color:red"></asp:Label>
+        </div>
+        <div>
+            <asp:Button ID="filterBtn" runat="server" Text="Filter" OnClick="filterBtn_Click"/>
+        </div>
+        <div class="clear"></div>
+        <div>
+            <asp:gridview id="EmployeeGridView"  
+        autogeneratecolumns="false" cellpadding="3" font-names="Helvetica" font-size="Larger"  gridlines="Both" 
+        runat="server" BackColor="#219ebc">  
+                <AlternatingRowStyle BackColor="#caf0f8" /> 
+                <HeaderStyle BackColor="#23047" Font-Bold="True" ForeColor="#F7F7F7" />
+                <PagerStyle BackColor="#E7E7FF" ForeColor="#4A3C8C" HorizontalAlign="Right" />
+        <columns>
+          <asp:boundfield datafield="id"
+            headertext="Employee ID" />
+          <asp:boundfield datafield="name"
+            headertext="Employee Name"/>
+          <asp:boundfield datafield="email"
+            headertext="Email"/>
+          <asp:boundfield datafield="dept"
+            headertext="Department"/>
+          <asp:boundfield datafield="contact"
+            headertext="Phone Number"/>
+            <asp:commandfield ShowDeleteButton="true" ShowEditButton="True"
+            headertext="Edit Controls"/>
+        
+        </columns>
+        
+      </asp:gridview>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Employee_Data
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["dispList"] == null && !IsPostBack)
+            {
+                Session["dispList"] = dispList;
+                EmployeeGridView.DataSource = Session["dispList"];
+                DataBind();
+            }
+            else
+            {
+                EmployeeGridView.DataSource = Session["dispList"];
+                DataBind();
+            }
+        }
+
+
+        List<Employees> dispList = new Employees().GetEmpList();
+
+        public class Employees
+        {
+            public string name { get; set; }
+            public string id { get; set; }
+            public string email { get; set; }
+            public string contact { get; set; }
+            public string dept { get; set; }
+
+            public List<Employees> GetEmpList()
+            {
+                Random rand = new Random();
+                var myList = new List<Employees>();
+                myList.Add(new Employees() { name = "Ali", id = "01" + rand.Next(10, 99).ToString(), email = "ali@rushtub.com", contact = "0301-64322345", dept = "Artificial Intelligence" });
+                myList.Add(new Employees() { name = "Hamza", id = "02" + rand.Next(10, 99).ToString(), email = "hamza@rushtub.com", contact = "0301-64322345", dept = "Artificial Intelligence" });
+                myList.Add(new Employees() { name = "Adil", id = "03" + rand.Next(10, 99).ToString(), email = "adil@rushtub.com", contact = "0301-64322345", dept = "Artificial Intelligence" });
+                myList.Add(new Employees() { name = "Alia", id = "04" + rand.Next(10, 99).ToString(), email = "alia@rushtub.com", contact = "0301-64322345", dept = "Artificial Intelligence" });
+                myList.Add(new Employees() { name = "Halim", id = "05" + rand.Next(10, 99).ToString(), email = "Halim@rushtub.com", contact = "0301-64322345", dept = "Web Development" });
+                myList.Add(new Employees() { name = "Sarim", id = "06" + rand.Next(10, 99).ToString(), email = "sarim@rushtub.com", contact = "0301-64322345", dept = "Web Development" });
+                myList.Add(new Employees() { name = "Qasim", id = "07" + rand.Next(10, 99).ToString(), email = "qasim@rushtub.com", contact = "0301-64322345", dept = "Front End Development" });
+                myList.Add(new Employees() { name = "Hala", id = "08" + rand.Next(10, 99).ToString(), email = "hala@rushtub.com", contact = "0301-64322345", dept = "Front End Development" });
+                myList.Add(new Employees() { name = "Husnain", id = "09" + rand.Next(10, 99).ToString(), email = "husnain@rushtub.com", contact = "0301-64322345", dept = "Production Analyst" });
+                myList.Add(new Employees() { name = "Shehryar", id = "10" + rand.Next(10, 99).ToString(), email = "shehryar@rushtub.com", contact = "0301-64322345", dept = "Production Analyst" });
+                myList.Add(new Employees() { name = "Hamza", id = "11" + rand.Next(10, 99).ToString(), email = "hamza@rushtub.com", contact = "0301-64322345", dept = "Human Resource" });
+                myList.Add(new Employees() { name = "Hania", id = "12" + rand.Next(10, 99).ToString(), email = "hania@rushtub.com", contact = "0301-64322345", dept = "Human Resource" });
+                myList.Add(new Employees() { name = "Abdullah", id = "13" + rand.Next(10, 99).ToString(), email = "abdullah@rushtub.com", contact = "0301-64322345", dept = "Human Resource" });
+                myList.Add(new Employees() { name = "Asim", id = "14" + rand.Next(10, 99).ToString(), email = "asim@rushtub.com", contact = "0301-64322345", dept = "Quality Assurance" });
+                myList.Add(new Employees() { name = "Dawar", id = "15" + rand.Next(10, 99).ToString(), email = "dawar@rushtub.com", contact = "0301-64322345", dept = "Quality Assurance" });
+                return myList;
+            }
+
+        }
+
+        protected void filterBtn_Click(object sender, EventArgs e)
+        {
+            var userInput = filter.Text;
+            List<Employees> options = new List<Employees>();
+            List<Employees> employees = Session["dispList"] as List<Employees>;
+            foreach (Employees obj in employees)
+            {
+                if (((obj.dept).ToLower()).StartsWith((userInput).ToLower()))
+                {
+                    options.Add(obj);
+                }
+            }
+            Session["dispList"] = options;
+            Response.Redirect("WebForm1.aspx");
+
+
+        }
+
+        [WebMethod]
+        public List<string> DisplayOptions(string userInput)
+        {
+            List<string> options = new List<string>();
+            List<Employees> employees = Session["dispList"] as List<Employees>;
+            foreach (var obj in employees)
+            {
+                if (((obj.dept).ToLower()).StartsWith(userInput.ToLower()))
+                {
+                    options.Add(obj.dept);
+                }
+            }
+            return options;
+        }
+    }
+}
